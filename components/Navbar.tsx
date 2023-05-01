5af6c920e62ce47054483952b51b66d6cabc107b
Luca Protelli <lucaprotelli23@icloud.com>
2023-05-01 21:53:44 +0200

    adding auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,23 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { AiOutlineGithub } from 'react-icons/ai'
+import Image from "next/image"
+import Link from "next/link"
+import { UserButton } from "@clerk/nextjs"
 
 export default function Navbar() {
-  return (
-    <header className="flex justify-between items-center w-full mt-5 border-b-2 border-stone-400 pb-7 sm:px-4 px-2">
-      <Link href="/" className="flex space-x-2 items-center">
-        <Image
-          alt="header text"
-          src="/logo.png"
-          className="sm:w-14 sm:h-14 w-9 h-9"
-          width={36}
-          height={36}
-        />
-        <h1 className="sm:text-5xl text-3xl font-bold ml-2 tracking-tight text-white hover:text-white/50">
-          CaptionAI
-        </h1>
-      </Link>
-    </header>
-  )
+    return (
+        <header className="flex justify-between items-center w-full mt-5 border-b-2 border-stone-400 pb-7 sm:px-4 px-2">
+            <Link href="/" className="flex space-x-2 items-center">
+                <Image
+                    alt="header text"
+                    src="/logo.png"
+                    className="sm:w-14 sm:h-14 w-9 h-9"
+                    width={36}
+                    height={36}
+                />
+                <h1 className="sm:text-5xl text-3xl font-bold ml-2 tracking-tight text-white hover:text-white/50">
+                    CaptionAI
+                </h1>
+            </Link>
+            <UserButton />
+        </header>
+    )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,51 @@
-import { Analytics } from "@vercel/analytics/react";
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import type { AppProps } from "next/app"
+import "../styles/globals.css"
+import {
+    ClerkProvider,
+    SignedIn,
+    SignedOut,
+    RedirectToSignIn,
+} from "@clerk/nextjs"
+import { useRouter } from "next/router"
+
+//  List pages you want to be publicly accessible, or leave empty if
+//  every page requires authentication. Use this naming strategy:
+//   "/"              for pages/index.js
+//   "/foo"           for pages/foo/index.js
+//   "/foo/bar"       for pages/foo/bar.js
+//   "/foo/[...bar]"  for pages/foo/[...bar].js
+const publicPages: Array<string> = [
+    "/sign-in/[[...index]]",
+    "/sign-up/[[...index]]",
+]
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
+    // Get the pathname
+    const { pathname } = useRouter()
+
+    // Check if the current route matches a public page
+    const isPublicPage = publicPages.includes(pathname)
+
+    // If the current route is listed as public, render it directly
+    // Otherwise, use Clerk to require authentication
+    return (
+        <ClerkProvider {...pageProps}>
+            {isPublicPage ? (
+                <Component {...pageProps} />
+            ) : (
+                <>
+                    <SignedIn>
+                        <Component {...pageProps} />
+                        <Analytics />
+                    </SignedIn>
+                    <SignedOut>
+                        <RedirectToSignIn />
+                    </SignedOut>
+                </>
+            )}
+        </ClerkProvider>
+    )
 }
 
-export default MyApp;
+export default MyApp
